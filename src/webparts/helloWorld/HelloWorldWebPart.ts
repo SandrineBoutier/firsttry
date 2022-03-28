418fced74d2632fd5a47429a5c0fa69b4e774cb5
s.boutier <s.boutier@LGOP4422>
2022-03-28 13:29:33 +0200

    modification innerHTLM

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -34,34 +34,22 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
 
   public render(): void {
     this.domElement.innerHTML = `
-    <section class="${styles.helloWorld} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-      <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-          <p>${escape(this.properties.test)}</p>
-          <p>${this.properties.test1}</p>
-          <p>${escape(this.properties.test2)}</p>
-          <p>${this.properties.test3}</p>
+    <div class="${ styles.helloWorld }">
+      <div class="${ styles.container }">
+        <div class="${ styles.row }">
+          <div class="${ styles.column }">
+            <span class="${ styles.title }">Welcome to SharePoint!</span>
+            <p class="${ styles.subTitle }">Customize SharePoint experiences using web parts.</p>
+            <p class="${ styles.description }">${escape(this.properties.description)}</p>
+            <p class="${ styles.description }">${escape(this.properties.test)}</p>
+            <p class="${ styles.description }">Loading from ${escape(this.context.pageContext.web.title)}</p>
+            <a href="https://aka.ms/spfx" class="${ styles.button }">
+              <span class="${ styles.label }">Learn more</span>
+            </a>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
-      </div>
-    </section>`;
+    </div>`;
   }
 
   private _getEnvironmentMessage(): string {
